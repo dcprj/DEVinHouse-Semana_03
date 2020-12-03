12fc4d3132135b42430d308280eab80b393568e7
Dario Pinheiro <dpinheiro@gmail.com>
2020-12-02 23:34:14 -0300

    Estrutura HTML pronta. Checagem das tarefas com JS funcionando

---
+++ projeto-todolist/scripts/script.js
@@ -0,0 +1,18 @@
+var caixasSelecao = document.getElementsByClassName('chk-status');
+console.log(caixasSelecao);
+
+for (let i = 0; i < caixasSelecao.length; i++) {
+    caixasSelecao[i].addEventListener('click', function () {
+
+        if (caixasSelecao[i].checked) {
+            id = caixasSelecao[i].id;
+            id = id.replace('chk-', 'sp-');
+            document.getElementById(id).style.textDecoration = 'line-through'
+        } else {
+            id = caixasSelecao[i].id;
+            id = id.replace('chk-', 'sp-');            
+            document.getElementById(id).style.textDecoration = 'none'
+        }
+
+    });
+}
