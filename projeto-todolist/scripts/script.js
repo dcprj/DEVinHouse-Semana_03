bfa596e7bc00d7ff919d70ac33c1a9bdb6c59675
Dario Pinheiro <dpinheiro@gmail.com>
2020-12-02 23:43:00 -0300

    Refatoracao do JS

--- projeto-todolist/scripts/script.js
+++ projeto-todolist/scripts/script.js
@@ -3,15 +3,12 @@ console.log(caixasSelecao);
 
 for (let i = 0; i < caixasSelecao.length; i++) {
     caixasSelecao[i].addEventListener('click', function () {
-
         if (caixasSelecao[i].checked) {
-            id = caixasSelecao[i].id;
-            id = id.replace('chk-', 'sp-');
-            document.getElementById(id).style.textDecoration = 'line-through'
+            id = caixasSelecao[i].id.replace('chk-', 'sp-');
+            document.getElementById(id).style.textDecoration = 'line-through';
         } else {
-            id = caixasSelecao[i].id;
-            id = id.replace('chk-', 'sp-');            
-            document.getElementById(id).style.textDecoration = 'none'
+            id = caixasSelecao[i].id.replace('chk-', 'sp-');
+            document.getElementById(id).style.textDecoration = 'none';
         }
 
     });
