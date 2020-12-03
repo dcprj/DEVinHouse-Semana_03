5ebaed3b9a020c513778e4eb13d1bced8f004e65
Dario Pinheiro <dpinheiro@gmail.com>
2020-12-03 07:52:21 -0300

    Rafatoracao JS no trecho que ouve os caixas de selecao

--- projeto-todolist/scripts/script.js
+++ projeto-todolist/scripts/script.js
@@ -1,15 +1,14 @@
 var caixasSelecao = document.getElementsByClassName('chk-status');
-console.log(caixasSelecao);
 
+// Ouvindo as caixas de seleção para marcar ou desmarcar as tarefas.
 for (let i = 0; i < caixasSelecao.length; i++) {
+    let id_elemento = null;
+    id_elemento = caixasSelecao[i].id.replace('chk-', 'sp-');
     caixasSelecao[i].addEventListener('click', function () {
-        if (caixasSelecao[i].checked) {
-            id = caixasSelecao[i].id.replace('chk-', 'sp-');
-            document.getElementById(id).style.textDecoration = 'line-through';
-        } else {
-            id = caixasSelecao[i].id.replace('chk-', 'sp-');
-            document.getElementById(id).style.textDecoration = 'none';
+        if (caixasSelecao[i].checked) {            
+            document.getElementById(id_elemento).style.textDecoration = 'line-through';
+        } else {            
+            document.getElementById(id_elemento).style.textDecoration = 'none';
         }
-
     });
 }
